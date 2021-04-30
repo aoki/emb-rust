6313d68feed15b56c52cfcb6f3860bc050a5f306
aoki <ringohub@gmail.com>
2021-04-30 22:51:03 +0900

    Learn macro

--- app/my_module/src/main.rs
+++ app/my_module/src/main.rs
@@ -1,6 +1,18 @@
 mod new_module;
 
+#[derive(Debug)]
+struct Sensor {
+    active: bool,
+    latest: u32,
+}
+
 fn main() {
+    let s = Sensor {
+        active: true,
+        latest: 42,
+    };
+
+    println!("{:?}", s);
     new_module::public_func();
     println!("Hello, world!");
 }
